d665c6b4d199c4b5ee4cdf9fd8aae1b7fa0e99de
colinmichael89 <colinmichael89@gmail.com>
2022-08-28 18:22:25 -0600

    all tests running

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -1,1 +1,26 @@
 const Engineer = require(`../lib/Engineer`);
+
+const engineer = new Engineer(
+  `Colin`,
+  `12345`,
+  `colinmichael89@gmail.com`,
+  `colinmichael89`
+);
+
+test(`Return the values from the engineer object`, () => {
+  expect(engineer.name).toBe(`Colin`);
+  expect(engineer.id).toBe(`12345`);
+  expect(engineer.email).toBe(`colinmichael89@gmail.com`);
+});
+test(`Return name from getName() method`, () => {
+  expect(engineer.getName()).toBe(`Colin`);
+});
+test(`Return id from getid() method`, () => {
+  expect(engineer.getId()).toBe(`12345`);
+});
+test(`Return email from getEmail() method`, () => {
+  expect(engineer.getEmail()).toBe(`colinmichael89@gmail.com`);
+});
+test(`Return github from getGithub() method`, () => {
+  expect(engineer.getGithub()).toBe(`colinmichael89`);
+});
--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -1,1 +1,26 @@
 const Intern = require(`../lib/Intern`);
+
+const intern = new Intern(
+  `Colin`,
+  `12345`,
+  `colinmichael89@gmail.com`,
+  `University of Denver`
+);
+
+test(`Return the values from the Intern object`, () => {
+  expect(intern.name).toBe(`Colin`);
+  expect(intern.id).toBe(`12345`);
+  expect(intern.email).toBe(`colinmichael89@gmail.com`);
+});
+test(`Return name from getName() method`, () => {
+  expect(intern.getName()).toBe(`Colin`);
+});
+test(`Return id from getid() method`, () => {
+  expect(intern.getId()).toBe(`12345`);
+});
+test(`Return email from getEmail() method`, () => {
+  expect(intern.getEmail()).toBe(`colinmichael89@gmail.com`);
+});
+test(`Return school from getSchool() method`, () => {
+  expect(intern.getSchool()).toBe(`University of Denver`);
+});
